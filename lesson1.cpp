48821088e57a699e9803a7b896ce81e5eabca2e4
londontown <londontown@163.com>
2013-04-19 17:26:41 +0800

    s

--- lesson1.cpp
+++ lesson1.cpp
@@ -3,8 +3,28 @@
 #include <glew.h>
 #include <glut.h>
 
+bool CreateGLWindow(char *title, int width, int height,
+		int bits) {
+	WNDCLASS Wnd;
+
+	Wnd.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	Wnd.lpfnWndProc = (WNDPROC) WndProc;
+	Wnd.cbClsExtra = 0;
+	Wnd.cbWndExtra = 0;
+	Wnd.hInstance = GetModuleHandle(NULL);
+	Wnd.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+	Wnd.hCursor = LoadCursor(NULL, IDC_ARROW);
+	Wnd.hbrBackground = NULL;
+	Wnd.lpszMenuName = NULL;
+	Wnd.lpszClassName = "londontown";
+
+	if (!RegisterClass(&Wnd)) {
+		return false;
+	}
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg,
-						WPARAM wParam, LPARAM lParam) {
+		WPARAM wParam, LPARAM lParam) {
 	switch (uMsg) {
 		case WM_SYSCOMMAND: {
 			switch (wParam) {
@@ -27,10 +47,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg,
 			return 0;
 		}
 	}
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance,
-                LPSTR lpCmdLine, int nCmdShow) {
+		LPSTR lpCmdLine, int nCmdShow) {
 	MSG msg;
 	//bool     
 
